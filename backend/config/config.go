627f5c2a6ba309a7b53ce85bd45528d01ddc4bd9
Carlos Sandoval <cdavidsv6@gmail.com>
2025-10-31 01:44:43 -0600

    Bump version to 0.3.0 in backend and frontend configuration

--- backend/config/config.go
+++ backend/config/config.go
@@ -13,7 +13,7 @@ var (
 /_/   /_/_/ .___/     /_/   /_/\____/ .___/
          /_/                       /_/      `
 
-	Version = "0.2.0"
+	Version = "0.3.0"
 	Host    = ":8000"
 
 	APILogLevel = log.INFO
